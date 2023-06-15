e34f05c6114a4430b9470abd6f47942bc6c59550
Yuvraj Sinha <123964429+yuvraj00001@users.noreply.github.com>
2023-06-15 21:18:52 +0530

    Initial commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+mongoose.connect('mongodb+srv://aragye497:aragye497@cluster1.zcg4zmp.mongodb.net/?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 
